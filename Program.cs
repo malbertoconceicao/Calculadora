137e6f628fe54fe1ac548bf38955c8e3aefbabfb
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-14 10:23:48 -0300

    sextaAula

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Divisao();
+            Multiplicacao();
         }
         static void Soma()
         {
@@ -58,5 +58,20 @@ namespace Calculator
             Console.ReadKey();
 
         }
+    static void Multiplicacao()
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float resultado = v1 * v2;
+        Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+        Console.ReadKey();
+    }
     }
 }
