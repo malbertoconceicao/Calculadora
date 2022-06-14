7aec55279cafd18fa71fa8b4fd28bf0022530293
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-14 10:12:19 -0300

    QuartaAula

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
         static void Soma()
         {
@@ -21,8 +21,25 @@ namespace Calculator
             float  resultado = v1 + v2;
            // Console.WriteLine("O resultado da soma é " + resultado);
             Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
            // Console.WriteLine($"O resultado da soma é {v1 + v2}");
            // Console.WriteLine($"O resultado da soma é " + (v1 + v2));
         }
+        static void Subtracao(){
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float  resultado = v1 - v2;
+           // Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+    
     }
 }
